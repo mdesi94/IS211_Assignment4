00cc510fdbeea4972dcb0b7300b5837d7d8a87f8
mdesi94 <marissa.desimone1@gmail.com>
2022-02-27 11:27:44 -0500

    Update sort_compare.py

--- sort_compare.py
+++ sort_compare.py
@@ -1,6 +1,5 @@
 import argparse
-# other imports go here
-
+import time
 import random
 
 def get_me_random_list(n):
@@ -15,16 +14,81 @@ def get_me_random_list(n):
     
 
 def insertion_sort(a_list):
-    pass
+    for index in range(1, len(a_list)):
+
+        currentvalue = a_list[index]
+        position = index
+
+        while position>0 and a_list[position-1]>currentvalue:
+            a_list[position]=a_list[position-1]
+            position = position-1
+
+        a_list[position] = currentvalue
 
 
 def shell_sort(a_list):
-    pass
+    sublistcount = len(a_list) // 2
+    while sublistcount > 0:
+
+        for startposition in range(sublistcount):
+            gapInsertionSort(a_list, startposition, sublistcount)
+
+        sublistcount = sublistcount // 2
+
+
+def gapInsertionSort(a_list, start, gap):
+    for i in range(start + gap, len(a_list), gap):
+
+        currentvalue = a_list[i]
+        position = i
+
+        while position >= gap and a_list[position - gap] > currentvalue:
+            a_list[position] = a_list[position - gap]
+            position = position - gap
+
+        a_list[position] = currentvalue
 
 
 def python_sort(a_list):
-    pass
+    return sorted(a_list)
+
 
 if __name__ == "__main__":
     """Main entry point"""
-    pass
+
+#generate 100 random lists and determine avg time to run for each list size using insertion sort
+
+list_sizes = [200, 500, 1000]
+for size in list_sizes:
+    total_time = 0
+    for i in range(100):
+        start = time.time()
+        insertion_sort(get_me_random_list(size))
+        time_spent = time.time() - start
+        total_time += time_spent
+    avg_time = total_time / 100
+    print(f"insertion sort took {avg_time:10.7f} seconds to run, on average for a list of {size} elements.")
+
+#generate 100 random lists and determine avg time to run for each list size using shell sort
+list_sizes = [200, 500, 1000]
+for size in list_sizes:
+    total_time = 0
+    for i in range(100):
+        start = time.time()
+        shell_sort(get_me_random_list(size))
+        time_spent = time.time() - start
+        total_time += time_spent
+    avg_time = total_time / 100
+    print(f"shell sort took {avg_time:10.7f} seconds to run, on average for a list of {size} elements.")
+
+#generate 100 random lists and determine avg time to run for each list size using builtin python sort
+list_sizes = [200, 500, 1000]
+for size in list_sizes:
+    total_time = 0
+    for i in range(100):
+        start = time.time()
+        python_sort(get_me_random_list(size))
+        time_spent = time.time() - start
+        total_time += time_spent
+    avg_time = total_time / 100
+    print(f"python sort took {avg_time:10.7f} seconds to run, on average for a list of {size} elements.")
